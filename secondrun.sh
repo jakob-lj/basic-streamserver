2b72552e04d9b0864ec24d482ff4521b44ae8209
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2021-02-23 01:20:03 +0100

    added stuff

--- secondrun.sh
+++ secondrun.sh
@@ -1,1 +1,1 @@
-docker run  -p 1935:1935 -p 80:80 -v $(pwd)/nginx/two.conf:/etc/nginx/nginx.conf nr
+docker run  -p 1935:1935 -p 80:80 -v $(pwd)/nginx/two.conf:/etc/nginx/nginx.conf -v $(pwd)/index.html:/var/www/jake/index.html nr
